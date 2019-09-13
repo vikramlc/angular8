9fd4b12511b5754688c1afaf5614fddfc342494d
Vikram Laxman Chawan <1994@solvezy.com>
2019-09-13 13:32:23 +0530

    Content child and passing data between components

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,19 +1,63 @@
-import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent implements OnInit {
+export class CockpitComponent
+  implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerContent = '';
   @ViewChild('serverContentInput', {static: false}) serverContentInput: ElementRef;
+  @ViewChild('heading', {static: true}) header: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
 
   ngOnInit() {
+    console.log('ngInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngDestroy called');
   }
 
   onAddServer(nameInput: HTMLInputElement) {
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, ContentChild, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  //encapsulation: ViewEncapsulation.None // Use when you don't want angular styles to be applied to only specific component
+  styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterViewInit, AfterContentInit {
 
   @Input() element: {type: string, name: string, content: string};
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: false}) contentParagraph: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    // console.log('Content child - ngInit: ' + this.contentParagraph.nativeElement.textContent);
   }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    // console.log('Content child - ngAfterContentInit: ' + this.contentParagraph.nativeElement.textContent);
+  }
+
 
 }
