eaf47008ac7788cdfd3a121e7a59be2206eb88eb
Vikram Laxman Chawan <1994@solvezy.com>
2019-09-16 21:22:58 +0530

    Observables module final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  isActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe(didActivate => {
+      this.isActivated = didActivate;
+    });
+  }
 }
