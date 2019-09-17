1cf3b13276861de6e8b8eafc18f3329e988406d9
Vikram Laxman Chawan <1994@solvezy.com>
2019-09-17 20:52:19 +0530

    Basics of http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+  isFetching = false;
+  error = null;
+
+  constructor(private http: HttpClient, private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+    });
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.isFetching = false;
+      this.error = error.message;
+    });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map, catchError } from 'rxjs/operators';
+import { Post } from './post.model';
+import { throwError } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(titleReceived: string, contentReceived: string) {
+
+    const postData: Post = {title: titleReceived, content: contentReceived};
+
+    // Send Http request
+    this.http
+      .post<{name: string}>('https://ng-angular-course-47745.firebaseio.com/posts.json', postData)
+      .subscribe((posts) => {
+        console.log(posts);
+      });
+  }
+
+  fetchPosts() {
+    return this.http.get<{ [key: string]: Post}>('https://ng-angular-course-47745.firebaseio.com/posts.json')
+      .pipe(
+        map (responseData => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key});
+            }
+          }
+          return postsArray;
+        }),
+        catchError(errorRes => {
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http.delete('https://ng-angular-course-47745.firebaseio.com/posts.json');
+  }
+}
