2cd1c2c0bd110374c9a5739e5c7514d35912c93d
Vikram Laxman Chawan <1994@solvezy.com>
2019-09-17 13:08:37 +0530

    Template driven approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+
+  genders = ['male', 'female'];
+  defaultQuestion = 'teacher';
+  answer = '';
+  users = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  isSubmitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+
+    // // Used to reset the entire form
+    // this.signupForm.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.users.username = this.signupForm.value.userData.username;
+    this.users.email = this.signupForm.value.userData.email;
+    this.users.secretQuestion = this.signupForm.value.secret;
+    this.users.answer = this.signupForm.value.questionAnswer;
+    this.users.gender = this.signupForm.value.gender;
+
+    // Resets the data once submitted in the UI input form
+    this.signupForm.reset();
+  }
+
 }
